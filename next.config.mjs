a834807c301aa117555466ec74d83272f6be0bbf
E-tika <107922336+E-tika@users.noreply.github.com>
2026-04-12 12:35:08 +0900

    next.config.mjs
    update 2026.4.12

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withExportImages from 'next-export-optimize-images';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
